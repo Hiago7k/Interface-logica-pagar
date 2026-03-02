69a37876ee11653c4e889bf45e52e66b2b19684e
Hiago 7k <hiago7k@gmail.com>
2026-03-02 19:02:56 -0300

    a

---
+++ Produto.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Interface_logica_pagar;
+
+internal class Produto
+{
+    public Produto(string nomeDoProduto, int quantidadeDoProduto, decimal valorDoProduto)
+    {
+        NomeDoProduto = nomeDoProduto;
+        QuantidadeDoProduto = quantidadeDoProduto;
+        ValorDoProduto = valorDoProduto;
+    }
+
+    public string NomeDoProduto { get; set; }
+    public int QuantidadeDoProduto { get; set; }
+    public decimal ValorDoProduto { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using Interface_logica_pagar;
+
+Produto produto1 = new Produto("Shampoo", 100, 20);
+Console.WriteLine($"Produto: {produto1.NomeDoProduto}");
+Console.WriteLine($"Quantidade disponivel: {produto1.QuantidadeDoProduto}");
+Console.WriteLine($"Valor: {produto1.ValorDoProduto}");
+
+Console.WriteLine();
+
+Servico servico1 = new Servico("Lavagem de Cabelo", 200, 30);
+Console.WriteLine($"Servico {servico1.NomeDoServico}");
+Console.WriteLine($"Valor do Servico {servico1.ValorDoServico}");
+Console.WriteLine($"Taxa da cabeleleira {servico1.TaxaDeServico}");
